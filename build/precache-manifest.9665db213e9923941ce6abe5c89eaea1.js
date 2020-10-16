992a9d67ce7ddb990b78705e8cd317a0631d0ac2
Kumar Gaurav <gauravpsp@gmail.com>
2020-10-16 22:08:08 +0530

    add deploy build

---
+++ build/precache-manifest.9665db213e9923941ce6abe5c89eaea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eaa3a98ee1a468afc49888248a84bf",
+    "url": "/Algorand-Wallet/index.html"
+  },
+  {
+    "revision": "c9c135e808bf3addda52",
+    "url": "/Algorand-Wallet/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83b8d702cea578ae2816",
+    "url": "/Algorand-Wallet/static/css/main.3b192cce.chunk.css"
+  },
+  {
+    "revision": "c9c135e808bf3addda52",
+    "url": "/Algorand-Wallet/static/js/2.69cdaace.chunk.js"
+  },
+  {
+    "revision": "d870a0ff8e5d1274b745f8885bdddcae",
+    "url": "/Algorand-Wallet/static/js/2.69cdaace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b8d702cea578ae2816",
+    "url": "/Algorand-Wallet/static/js/main.0fddc0c7.chunk.js"
+  },
+  {
+    "revision": "838d41b4b5af1c456ebb",
+    "url": "/Algorand-Wallet/static/js/runtime-main.3aea0540.js"
+  }
+]);
