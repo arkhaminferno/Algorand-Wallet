c17feef2b6b321955634ef752fad3e375dede74c
Kumar Gaurav <gauravpsp@gmail.com>
2020-10-16 21:56:41 +0530

    Revert "add deploy builds"
    This reverts commit 20ef1aace78cd9b6332d5d26991d351ce74f98b6.

--- src/components/toast/transactiontoast.js
+++ src/components/toast/transactiontoast.js
@@ -13,7 +13,7 @@ function transactiontoast(props) {
       <Toast.Header>
         <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
         <strong className="mr-auto">Success!</strong>
-        <small>2 seconds ago</small>
+        <small>3 seconds ago</small>
       </Toast.Header>
 
       <Toast.Body>
