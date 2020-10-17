35022538f1a02254b858e5aba5ef5cb4d6317b6d
Kumar Gaurav <gauravpsp@gmail.com>
2020-10-17 21:06:52 +0530

    Added import component description

--- src/components/modal/seedphraseInput.js
+++ src/components/modal/seedphraseInput.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+/**
+ * This component will take seed phrase as a input for importing account to the web app.
+ * @props toggleflag -> a boolean value for displaying the modal.
+ * @props onClick: -> this will submit typed/pasted mnemonic key  to the parent component.
+ * @props closeModal -> a boolean value for closing the Modal.
+ *
+ * @author [Kumar Gaurav](https://github.com/arkhaminferno)
+ */
 
 function seedphraseInput(props) {
-
-  const handleChange = event => {
+  const handleChange = (event) => {
     props.onChange(event.target.value);
-}
+  };
   return (
     <Modal show={props.toggleflag}>
       <Modal.Header>
